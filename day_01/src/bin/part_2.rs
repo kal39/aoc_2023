e289c139962a967f550c4975079d0169a3c7410f
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-12-01 11:34:39 +0100

    day 1 cleanup

--- day_01/src/bin/part_2.rs
+++ day_01/src/bin/part_2.rs
@@ -1,10 +1,6 @@
-use std::fs;
-use std::io;
-
-fn main() -> io::Result<()> {
-    let input = fs::read_to_string("input.txt")?;
-
-    let out = input
+fn main() {
+    let out = std::fs::read_to_string("input.txt")
+        .expect("failed to read input")
         .split("\n")
         .map(|s| s.to_string())
         .filter(|s| !s.is_empty())
@@ -18,13 +14,11 @@ fn main() -> io::Result<()> {
         .map(|s| s.replace("eight", "e8t"))
         .map(|s| s.replace("nine", "n9e"))
         .map(|s| s.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>())
-        .map(|s1| {
-            10 * s1.first().unwrap().to_digit(10).unwrap()
-                + s1.last().unwrap().to_digit(10).unwrap()
-        })
+        .map(|s| (s[0], s[s.len() - 1]))
+        .map(|s| (s.0.to_digit(10).unwrap(), s.1.to_digit(10).unwrap()))
+        .map(|s| 10 * s.0 + s.1)
         .sum::<u32>()
         .to_string();
 
     println!("result: {}", out);
-    Ok(())
 }
