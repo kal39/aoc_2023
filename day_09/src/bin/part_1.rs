d86f2b80095ebfa98fe28164f184fed28f8e02c0
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-12-09 15:26:33 +0100

    day 9

---
+++ day_09/src/bin/part_1.rs
@@ -0,0 +1,28 @@
+fn deriv(seq: &Vec<i32>) -> i32 {
+    if seq.iter().all(|v| *v == 0) {
+        return 0;
+    } else {
+        let mut d = Vec::new();
+        for i in 0..seq.len() - 1 {
+            d.push(seq[i + 1] - seq[i]);
+        }
+        return d.last().unwrap() + deriv(&d);
+    }
+}
+
+fn main() {
+    let res = std::fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| {
+            let seq = l
+                .split_whitespace()
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            seq.last().unwrap() + deriv(&seq)
+        })
+        .sum::<i32>();
+
+    println!("result: {}", res);
+}
