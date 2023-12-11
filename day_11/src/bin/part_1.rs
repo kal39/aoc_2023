df8e2bcb44854637081a7a7e8654451d92355334
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-12-11 07:29:45 +0100

    day 11

---
+++ day_11/src/bin/part_1.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let mut coords = Vec::new();
+
+    std::fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .enumerate()
+        .for_each(|(i, l)| {
+            l.chars().enumerate().for_each(|(j, c)| {
+                if c == '#' {
+                    coords.push((i as i32, j as i32));
+                }
+            });
+        });
+
+    println!("{:?}", coords);
+
+    let rows = coords.iter().map(|c| c.0).max().unwrap();
+    let cols = coords.iter().map(|c| c.1).max().unwrap();
+
+    for i in (0..rows).rev() {
+        if coords.iter().all(|&c| c.0 != i) {
+            coords.iter_mut().for_each(|c| {
+                if c.0 > i {
+                    c.0 += 1;
+                }
+            });
+        }
+    }
+
+    for i in (0..cols).rev() {
+        if coords.iter().all(|&c| c.1 != i) {
+            coords.iter_mut().for_each(|c| {
+                if c.1 > i {
+                    c.1 += 1;
+                }
+            });
+        }
+    }
+
+    println!("{:?}", coords);
+
+    let mut res = 0;
+    for i in 0..coords.len() {
+        for j in (i + 1)..coords.len() {
+            res += (coords[i].0 - coords[j].0).abs() + (coords[i].1 - coords[j].1).abs();
+        }
+    }
+
+    println!("result: {}", res);
+}
