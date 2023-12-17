f5e1c925f9cabf49353f963e4c45e0cfdbfc1ca7
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-12-17 11:26:46 +0100

    day 16

---
+++ src/bin/day_16_part_1.rs
@@ -0,0 +1,117 @@
+use std::collections::HashSet;
+
+fn get(map: &Vec<Vec<char>>, i: i32, j: i32) -> char {
+    if i >= 0 && i < map.len() as i32 && j >= 0 && j < map[i as usize].len() as i32 {
+        map[i as usize][j as usize]
+    } else {
+        '.'
+    }
+}
+
+fn set(map: &mut Vec<Vec<HashSet<char>>>, i: i32, j: i32, c: char) {
+    if i >= 0 && i < map.len() as i32 && j >= 0 && j < map[i as usize].len() as i32 {
+        map[i as usize][j as usize].insert(c);
+    }
+}
+
+fn main() {
+    let map = std::fs::read_to_string("input/day_16.txt")
+        .unwrap()
+        .lines()
+        .map(|s| s.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let mut state = map
+        .iter()
+        .map(|v| {
+            v.iter()
+                .map(|_| HashSet::new())
+                .collect::<Vec<HashSet<char>>>()
+        })
+        .collect::<Vec<Vec<HashSet<char>>>>();
+
+    state[0][0].insert('>');
+
+    let mut changed = true;
+    while changed {
+        let mut new_state = state.clone();
+
+        for i in 0..state.len() {
+            for j in 0..state[i].len() {
+                let i = i as i32;
+                let j = j as i32;
+
+                state[i as usize][j as usize].iter().for_each(|c| match c {
+                    '>' => match get(&map, i, j) {
+                        '.' => set(&mut new_state, i, j + 1, '>'),
+                        '/' => set(&mut new_state, i - 1, j, '^'),
+                        '\\' => set(&mut new_state, i + 1, j, 'v'),
+                        '|' => {
+                            set(&mut new_state, i - 1, j, '^');
+                            set(&mut new_state, i + 1, j, 'v');
+                        }
+                        '-' => set(&mut new_state, i, j + 1, '>'),
+                        _ => panic!(),
+                    },
+                    '<' => match get(&map, i, j) {
+                        '.' => set(&mut new_state, i, j - 1, '<'),
+                        '/' => set(&mut new_state, i + 1, j, 'v'),
+                        '\\' => set(&mut new_state, i - 1, j, '^'),
+                        '|' => {
+                            set(&mut new_state, i - 1, j, '^');
+                            set(&mut new_state, i + 1, j, 'v');
+                        }
+                        '-' => set(&mut new_state, i, j - 1, '<'),
+                        _ => panic!(),
+                    },
+                    '^' => match get(&map, i, j) {
+                        '.' => set(&mut new_state, i - 1, j, '^'),
+                        '/' => set(&mut new_state, i, j + 1, '>'),
+                        '\\' => set(&mut new_state, i, j - 1, '<'),
+                        '|' => set(&mut new_state, i - 1, j, '^'),
+                        '-' => {
+                            set(&mut new_state, i, j - 1, '<');
+                            set(&mut new_state, i, j + 1, '>');
+                        }
+                        _ => panic!(),
+                    },
+                    'v' => match get(&map, i, j) {
+                        '.' => set(&mut new_state, i + 1, j, 'v'),
+                        '/' => set(&mut new_state, i, j - 1, '<'),
+                        '\\' => set(&mut new_state, i, j + 1, '>'),
+                        '|' => set(&mut new_state, i + 1, j, 'v'),
+                        '-' => {
+                            set(&mut new_state, i, j - 1, '<');
+                            set(&mut new_state, i, j + 1, '>');
+                        }
+                        _ => panic!(),
+                    },
+                    '.' => (),
+                    c => panic!("found {} at {} {}", c, i, j),
+                });
+            }
+        }
+
+        changed = false;
+        for i in 0..state.len() {
+            for j in 0..state[i].len() {
+                if state[i][j] != new_state[i][j] {
+                    changed = true;
+                }
+            }
+        }
+
+        state = new_state;
+    }
+
+    let mut res = 0;
+    for i in 0..state.len() {
+        for j in 0..state[i].len() {
+            if state[i][j].len() > 0 {
+                res += 1;
+            }
+        }
+    }
+
+    println!("result: {}", res);
+}
